14048c0551785f81e329c1616df6fdf623490b16
tenhold <tenhold@gmail.com>
2020-11-07 02:13:39 -0600

    fix spelling error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 const Index = () => {
   return (
     <div>
-      <h1>Congraduations!</h1>
+      <h1>Congratulations!</h1>
       <h2>You've just created a React app and deployed with CodeStar!</h2>
       <h2>Great Job!</h2>
     </div>
